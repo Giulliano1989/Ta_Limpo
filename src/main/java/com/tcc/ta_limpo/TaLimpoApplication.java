2652bc889a7c20fb473921c5c90ef7544197654b
Giulliano <giulli_gf@hotmail.com>
2026-07-14 20:20:37 -0300

    incluido no Spring

---
+++ src/main/java/com/tcc/ta_limpo/TaLimpoApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.ta_limpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaLimpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaLimpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/ta_limpo/TaLimpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.ta_limpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaLimpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
